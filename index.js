18c062cd1fa98fdab0fc66a7138fbd9b901af85a
Eli White <github@eli-white.com>
2016-06-15 15:21:49 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*eslint-env es6*/
+// /*eslint no-var: "error"*/ // uncomment this line to verify that it is linting the file at all
+/*eslint object-shorthand: ["error", "always", { "avoidQuotes": true }]*/
+
+var foo = {
+    bar: function() {}
+};
+
+module.exports = foo;
